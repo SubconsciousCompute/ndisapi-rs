41531b387ca60405cf54d4199c3dd2d7a8db3543
Dilawar Singh <dilawar@subcom.tech>
2024-04-29 16:56:31 +0530

    chore: cargo +nightly fmt

--- src/ndisapi/static_api.rs
+++ src/ndisapi/static_api.rs
@@ -91,7 +91,8 @@ impl Ndisapi {
                 0,
                 KEY_READ,
                 &mut target_key,
-            ).ok()
+            )
+            .ok()
         };
 
         if result.is_err() {
@@ -313,12 +314,7 @@ impl Ndisapi {
                 KEY_READ,
                 &mut hkey,
             )
-<<<<<<< HEAD
-        }
-        .ok();
-=======
         }.ok();
->>>>>>> fa264a1 (chore: Fix breaking changes caused by https://github.com/microsoft/windows-rs/pull/2890)
 
         let mut value_type = REG_VALUE_TYPE::default();
         let mut data = vec![0u16; 256];
@@ -335,12 +331,8 @@ impl Ndisapi {
                     Some(data.as_mut_ptr() as *const u8 as *mut u8),
                     Some(&mut data_size),
                 )
-<<<<<<< HEAD
-            }
             .ok();
-=======
-            }.ok();
->>>>>>> fa264a1 (chore: Fix breaking changes caused by https://github.com/microsoft/windows-rs/pull/2890)
+            }
 
             if result.is_ok() {
                 friendly_name = if let Ok(name) = String::from_utf16(&data[..data_size as usize]) {
@@ -383,12 +375,7 @@ impl Ndisapi {
                 KEY_WRITE,
                 &mut hkey,
             )
-<<<<<<< HEAD
-        }
         .ok();
-=======
-        }.ok();
->>>>>>> fa264a1 (chore: Fix breaking changes caused by https://github.com/microsoft/windows-rs/pull/2890)
 
         if result.is_ok() {
             result = unsafe {
@@ -399,12 +386,8 @@ impl Ndisapi {
                     REG_DWORD,
                     Some(mtu_decrement.to_ne_bytes().as_ref()),
                 )
-<<<<<<< HEAD
             }
             .ok();
-=======
-            }.ok();
->>>>>>> fa264a1 (chore: Fix breaking changes caused by https://github.com/microsoft/windows-rs/pull/2890)
         }
 
         result
